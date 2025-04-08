35d83ce745caa239b5f66db653616db0b71528e0
simongottipalli <simon.gottipalli@gmail.com>
2025-04-08 21:26:28 +0200

    cleanup key

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { APIProvider } from '@vis.gl/react-google-maps';
 import BysykkelMap from "./components/BysykkelMap.tsx";
 
 const App = () => {
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    if (!apiKey) {
+        console.error('Google Maps API key is not set. Please set it in your environment variables.');
+        return <div>Error: Google Maps API key is not set.</div>;
+    }
     return (
-    <APIProvider apiKey={'AIzaSyC0xcBuSXYxhkonb8jYYXql_gNUIotFVZM'} onLoad={() => console.log('Maps API has loaded.')}>
+    <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
         <BysykkelMap />
     </APIProvider>
 );
